207b2245f1ff5b458844dd589a8abe3076a0d831
ahalyha <halyha@textkernel.nl>
2023-08-30 13:26:21 +0200

    initial

---
+++ nisaba/database.py
@@ -0,0 +1,89 @@
+import pinecone
+from langchain.document_loaders.sitemap import SitemapLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+pinecone.init(
+    api_key="d37388e3-0b31-4fa3-b810-599867aee2a4",  # find at app.pinecone.io
+    environment="gcp-starter"  # next to api key in console
+)
+# TODO: move to the other file
+# loader = SitemapLoader(
+#     "https://www.textkernel.com/sitemap_index.xml"
+# )
+# docs = loader.load()
+
+# text_splitter = RecursiveCharacterTextSplitter(
+#     chunk_size = 1200,
+#     chunk_overlap  = 200,
+#     length_function = len,
+# )
+#docs_chunks = text_splitter.split_documents(docs)
+embeddings = OpenAIEmbeddings()
+index_name = "ind"
+
+# #create a new index
+#docsearch = Pinecone.from_documents(docs_chunks, embeddings, index_name=index_name)
+
+
+# if you already have an index, you can load it like this
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+from langchain.llms import OpenAI
+llm=OpenAI()
+qa_with_sources = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever(), return_source_documents=True)
+
+
+def ask_a_question(request):
+    embeddings = OpenAIEmbeddings()
+    index_name = "ind"
+   
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+    llm=OpenAI()
+    qa_with_sources = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever(), return_source_documents=True)
+    result = qa_with_sources({"query": request})
+    return format_slack_message(result)
+
+def format_slack_message(response):
+    query = response['query']
+    result = response['result']
+    source_documents = response['source_documents']
+
+    message = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*What I see:*\n{result}"
+                }
+            }
+        ]
+    }
+
+    for document in source_documents:
+        page_content = document.page_content
+        metadata = document.metadata
+
+        if 'loc' in metadata:
+            link = metadata['loc']
+            message['blocks'].append({
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"<{link}|{link}>"
+                }
+            })
+
+    return message
+
+
+print(ask_a_question("Tell me about Textkernel products"))
---
+++ nisaba/slack.py
@@ -0,0 +1,59 @@
+import os
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
+from chat import send_to_openai
+from database import ask_a_question
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+
+# Initialize the Slack app
+app = App(token=SLACK_BOT_TOKEN)
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+@app.event("app_mention")
+def handle_mentions(body, say):
+    """
+    Event listener for mentions in Slack.
+    When the bot is mentioned, this function processes the text and sends a response.
+
+    Args:
+        body (dict): The event data received from Slack.
+        say (callable): A function for sending a response to the channel.
+    """
+    text = body["event"]["text"]
+
+    mention = f"<@{SLACK_BOT_USER_ID}>"
+    text = text.replace(mention, "").strip()
+
+    print(text)
+    say("Carefully searching... Wait a second")
+
+    response = ask_a_question(text)
+    #response = send_to_openai(text)
+    say(response)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    """
+    Route for handling Slack events.
+    This function passes the incoming HTTP request to the SlackRequestHandler for processing.
+
+    Returns:
+        Response: The result of handling the request.
+    """
+    return handler.handle(request)
+
+
+# Run the Flask app
+if __name__ == "__main__":
+    flask_app.run()
